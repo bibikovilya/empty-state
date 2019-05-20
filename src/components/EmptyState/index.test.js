077c6e98ae9c1f1a8a8460179d76dd38a213379a
Ilya Bibikov <ilya.bibikov@rubyroidlabs.com>
2019-05-20 14:48:22 +0300

    Add more tests.

--- src/components/EmptyState/index.test.js
+++ src/components/EmptyState/index.test.js
@@ -11,4 +11,38 @@ describe('EmptyState', () => {
 
     expect(component.toJSON()).toMatchSnapshot();
   });
+
+  it('should render children with data', () => {
+    const data = ['A', 'B', 'C']
+    const component = TestRenderer.create(
+      <EmptyState data={data}>
+        {data}
+      </EmptyState>
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render default empty state component', () => {
+    const data = []
+    const component = TestRenderer.create(
+      <EmptyState data={data}>
+        {data}
+      </EmptyState>
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render empty state component', () => {
+    const data = []
+    const fallback = <span>Empty state</span>
+    const component = TestRenderer.create(
+      <EmptyState data={data} fallback={fallback}>
+        {data}
+      </EmptyState>
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 });

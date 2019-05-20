4e4ca968bd5b6716514df583c459d44c62779423
Ilya Bibikov <ilya.bibikov@rubyroidlabs.com>
2019-05-20 12:10:48 +0300

    Update storybook config.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
 import { configure } from '@storybook/react';
 
+const req = require.context('../src/components', true, /stories\.js$/);
 function loadStories() {
+  req.keys().forEach(filename => req(filename));
   require('../src/stories');
 }
 

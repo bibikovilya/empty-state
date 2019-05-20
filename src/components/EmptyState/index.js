021e70fb0e2a601d28a145dc81f23c69dd4acf62
Ilya Bibikov <ilya.bibikov@rubyroidlabs.com>
2019-05-20 12:31:57 +0300

    Add empty state design.

--- src/components/EmptyState/index.js
+++ src/components/EmptyState/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import logo from '../../assets/chatchamp-logo.png';
+import './index.css';
+
+const EmptyComponent = () => (
+  <div className="card">
+    <p>We don't have what to show you</p>
+    <img className="card__logo" src={logo} alt="No data" />
+  </div>
+)
 
 const EmptyState = ({ data, children }) => (
   data.length === 0
-    ? <span>Empty</span>
+    ? <EmptyComponent />
     : children
 )
 
